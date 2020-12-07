75d09a3f6b178f6198df3698079d4877577a1e76
Chad Lineberry <lineberry@gmail.com>
2020-12-06 23:56:02 -0500

    Simplify a bit

--- Day-6/Program.fs
+++ Day-6/Program.fs
@@ -1,21 +1,9 @@
-﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-
-open System
+﻿open System
 open System.IO
 
 //Read the input file and turn it into an array of strings
 let readlines (filePath:string) = 
     File.ReadAllLines(filePath) |> Array.toList
-
-//Helper function to just print every character in a set
-let printSet (setToPrint:Set<char>) =
-    Set.iter (fun x -> printfn "%c," x) setToPrint
-
-//Add all characters on the line to a Set so you are only left with the unique characters
-let rec parseLine (charList:List<char>) (setAcc:Set<char>) =
-    match charList with
-        | head::tail -> parseLine tail (setAcc.Add(head))
-        | [] ->  setAcc                  
 
 //Parse the answers for part 1
 let rec parseAnswers (answerList:List<string>) (setAcc:Set<char>) (answerAcc:int) =
@@ -24,8 +12,8 @@ let rec parseAnswers (answerList:List<string>) (setAcc:Set<char>) (answerAcc:int
             if head.Length = 0 then //Start a new group.  The setAcc here has the uniqe answers for the group
                 parseAnswers tail Set.empty (answerAcc+setAcc.Count) 
             else
-                let answersSoFar = parseLine (head.ToCharArray() |> Array.toList) setAcc
-                parseAnswers tail answersSoFar answerAcc                        
+                let answersSoFar = Set.ofArray (head.ToCharArray())
+                parseAnswers tail (Set.union setAcc answersSoFar) answerAcc                        
         | [] -> (answerAcc+setAcc.Count)
 
 //Parse the answers for part 2
@@ -36,9 +24,8 @@ let rec parsePart2 (answerList:List<string>) (setAcc:List<Set<char>>) (answerAcc
                 let groupAnswer = Set.intersectMany setAcc
                 parsePart2 tail [] (answerAcc + groupAnswer.Count)
             else
-                let answers = parseLine (head.ToCharArray() |> Array.toList) Set.empty
-                let answerList = List.append setAcc [answers]
-                parsePart2 tail answerList answerAcc 
+                let answers = Set.ofArray (head.ToCharArray())
+                parsePart2 tail (List.append setAcc [answers]) answerAcc 
         | [] -> 
             let groupAnswer = Set.intersectMany setAcc
             (answerAcc + groupAnswer.Count)
@@ -52,5 +39,4 @@ let main argv =
 
     let answer2 = parsePart2 questionAnswers [] 0
     printfn "The answer for part 2 is %i." answer2
-
     0 // return an integer exit code

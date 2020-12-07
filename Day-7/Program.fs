89d6de0d1f194467d1e6c4e24287390027f29952
OWEN2\Setsy <chad@tcgplayer.com>
2020-12-07 17:14:07 -0500

    Some framework for day 7

--- Day-7/Program.fs
+++ Day-7/Program.fs
@@ -1,13 +1,48 @@
-﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+﻿open System
+open System.IO
 
-open System
+//Read the input file and turn it into an array of strings
+let readlines (filePath:string) = 
+    File.ReadAllLines(filePath) |> Array.toList
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let inline charToInt c = int c - int '0'
+
+//Extract the color bag that the rule applies to
+let extractRuleColor (rule:string) = 
+    rule.Split([|" bags"|], StringSplitOptions.RemoveEmptyEntries).[0]
+
+//Extract the rules that apply to each bag from the line
+let extractRules (rule:string) = 
+    rule.Split([|"contain "|], StringSplitOptions.RemoveEmptyEntries).[1].Split([|", "|], StringSplitOptions.RemoveEmptyEntries)
+        |> Array.map (fun x -> x.Replace(" bags", "").Replace(" bag","").Replace(".", ""))
+        |> Array.fold (fun (acc:Map<string,int>) elem -> acc.Add(elem.[2..], (elem.[0] |> charToInt ))) Map.empty
+
+let parseInput (bagRules:list<string>) =
+    List.fold (fun (acc:Map<string,Map<string,int>>) elem -> acc.Add(extractRuleColor elem, extractRules elem)) Map.empty bagRules
+
+let findBagsThatCanContainBag (bagColor:string) (parsedInput:Map<string,Map<string,int>>) =
+    Map.fold (fun (acc:Set<string>) k (v:Map<string,int>) ->  if v.ContainsKey(bagColor) then acc.Add(k) else acc) Set.empty parsedInput
+
+// let rec recursiveFindBags (bagColors:list<string>) (parsedInput:Map<string,Map<string,int>>) (recAcc:Set<string>) =
+//     match bagColors with
+//         | head::tail ->
+//             let thisBagContainers = findBagsThatCanContainBag head parsedInput
+//             let rec loop (c:Set<string>) recAcc =
+//                 match c with
+//                     | h::t -> 
+//                         let tbc = findBagsThatCanContainBag h parsedInput
+//                         loop t recAcc
+//                     | [] -> recAcc
+
+//             recursiveFindBags tail parsedInput (recAcc.(head))
+//         | [] -> recAcc
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    let bagRules = readlines "input.txt"
+    let allColors = parseInput bagRules
+    let shinyGoldContainers = findBagsThatCanContainBag "shiny gold"  allColors |> Set.toList
+
+    //let answer = recursiveFindBags shinyGoldContainers allColors Set.empty
+    //printfn "The answer is %i." answer.Count
     0 // return an integer exit code

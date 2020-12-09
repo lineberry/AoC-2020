0c787baa047f47f7ca3b36a4cca67889dd6df20a
OWEN2\Setsy <chad@tcgplayer.com>
2020-12-09 14:00:23 -0500

    Setup for day 10

---
+++ Day-10/Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+open System.IO
+
+//Read the input file and turn it into an array of strings
+let readlines (filePath:string) = 
+    File.ReadAllLines(filePath) |> Array.toList
+
+[<EntryPoint>]
+let main argv =
+    let input = readlines "input.txt"
+    0 // return an integer exit code

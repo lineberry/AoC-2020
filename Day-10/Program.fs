1c64de2c006417904b103bf1d4bfbb69862c4cb2
OWEN2\Setsy <chad@tcgplayer.com>
2020-12-10 13:04:20 -0500

    Solve day 10 part 1

--- Day-10/Program.fs
+++ Day-10/Program.fs
@@ -3,9 +3,24 @@ open System.IO
 
 //Read the input file and turn it into an array of strings
 let readlines (filePath:string) = 
-    File.ReadAllLines(filePath) |> Array.toList
+    File.ReadAllLines(filePath) |> Array.toList |> List.map int |> List.sort
+
+let generateAcc elem1 elem2 (tup:(int*int)) =
+    if elem2 - elem1 = 3 then
+        (fst tup, snd tup+1)
+    elif elem2 - elem1 = 1 then
+        (fst tup+1, snd tup)
+    else tup
 
 [<EntryPoint>]
 let main argv =
-    let input = readlines "input.txt"
+    let originalJoltageList = readlines "input.txt"
+    let builtInAdapter = List.max originalJoltageList + 3
+    let chainToDevice = originalJoltageList @ [builtInAdapter]
+    let chainFromOutlet = 0::originalJoltageList
+
+    let joltageDifferences = List.fold2 (fun (acc:(int*int)) elem1 elem2 ->  generateAcc elem1 elem2 acc) (0,0) chainFromOutlet chainToDevice
+    printfn "The answer to part 1 is %i." (fst joltageDifferences * snd joltageDifferences)
+
+    //List.iter (fun x -> printfn "%" x) test
     0 // return an integer exit code

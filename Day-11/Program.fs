743d9cd1346003676813cbeb59544dec779c142a
OWEN2\Setsy <chad@tcgplayer.com>
2020-12-11 15:46:19 -0500

    Get rid of some code duplciation for Day 11

--- Day-11/Program.fs
+++ Day-11/Program.fs
@@ -5,25 +5,30 @@ open System.IO
 let readlines (filePath:string) = 
     File.ReadAllLines(filePath) |> Array.map (fun x -> x.ToCharArray()) |> array2D
 
-//Helper function to visualize the seating chart
-let printGrid (grid:char[,]) =
-    let inputWidth = Array2D.length1 grid
-    let inputHeight = Array2D.length2 grid
+let isSeatIndexValid x y gridWidth gridHeight =
+    not (x < 0 || y < 0 || x > gridWidth - 1 || y > gridHeight - 1)
 
-    for x = 0 to (inputWidth-1) do
-        printfn ""
-        for y = 0 to (inputHeight-1) do
-            printf "%c" grid.[x,y]
+let rec findNearestSeat x y radius (slope:(int*int)) (grid:char[,]) =
+    let gridWidth = Array2D.length1 grid
+    let gridHeight = Array2D.length2 grid
+    let xSlope = fst slope * radius
+    let ySlope = snd slope * radius
+
+    if not (isSeatIndexValid (x+xSlope) (y+ySlope) gridWidth gridHeight) || grid.[x+xSlope,y+ySlope] <> '.' then (x+xSlope,y+ySlope)
+    else findNearestSeat x y (radius+1) slope grid
 
 //0,0 in the top left
 //y increasing down
-let getNeighborIndexes x y =
-    [x-1,y-1; x,y-1; x+1,y-1;
-     x-1,y;            x+1,y;
-     x-1,y+1; x,y+1;  x+1,y+1]
-
-let isSeatIndexValid x y gridWidth gridHeight =
-    not (x < 0 || y < 0 || x > gridWidth - 1 || y > gridHeight - 1)
+let getNeighborIndexes x y (grid:char[,]) partId =
+    if partId = 1 then
+        [x-1,y-1; x,y-1; x+1,y-1;
+         x-1,y;            x+1,y;
+         x-1,y+1; x,y+1;  x+1,y+1]
+    else
+        let slopes = [(-1,-1); (0,-1); (1,-1);
+                      (-1,0);           (1,0);
+                      (-1,1);  (0,1);   (1,1)]
+        List.map (fun s -> findNearestSeat x y 1 s grid) slopes 
 
 let isSeatOccupied x y (grid:char[,]) =
     if not (isSeatIndexValid x y (Array2D.length1 grid) (Array2D.length2 grid)) then 0
@@ -35,33 +40,39 @@ let isSeatOccupied x y (grid:char[,]) =
 let getCountOfOccupiedNeighbors (neighborIndexes:list<(int*int)>) (grid:char[,]) =
     List.sumBy (fun elem -> isSeatOccupied (fst elem) (snd elem) grid) neighborIndexes 
 
-let getNewSeatStatePart1 x y (grid:char[,]) =
+let getNewSeatState x y (grid:char[,]) partId =
     let currentState = grid.[x,y]
-    let neighborIndexes = getNeighborIndexes x y
+    let neighborIndexes = getNeighborIndexes x y grid partId
     let countOfOccupiedNeighbors = getCountOfOccupiedNeighbors neighborIndexes grid
+    let tooManyNeighbors = if partId = 1 then 4 else 5
 
     match countOfOccupiedNeighbors with
-    | x when currentState = '#' && x >= 4 -> 'L'
+    | x when currentState = '#' && x >= tooManyNeighbors -> 'L'
     | x when currentState = 'L' && x = 0 -> '#'
     | _ -> currentState
 
-let calcGameRoundPart1 (grid:char[,]) =
-    Array2D.init (Array2D.length1 grid) (Array2D.length2 grid) (fun x y -> getNewSeatStatePart1 x y grid)
+let calcGameRound (grid:char[,]) partId =
+    Array2D.init (Array2D.length1 grid) (Array2D.length2 grid) (fun x y -> getNewSeatState x y grid partId)
 
 let getTotalOccupiedSeats (grid:char[,]) =
     grid |> Seq.cast |> Seq.filter (fun x -> x = '#') |> Seq.length
 
-let rec gameLoopPart1 (prevRound:char[,]) =
-    let nextRound = calcGameRoundPart1 prevRound
+let rec gameLoop (prevRound:char[,]) partId =
+    let nextRound = calcGameRound prevRound partId
     if prevRound = nextRound then
         getTotalOccupiedSeats nextRound
-    else gameLoopPart1 nextRound
+    else gameLoop nextRound partId
 
 
 [<EntryPoint>]
 let main argv =
     let input = readlines "input.txt"
-    let answer = gameLoopPart1 input
 
+    //Solve part 1
+    let answer = gameLoop input 1
     printfn "The answer to part 1 is %i." answer
+
+    //Solve part 2
+    let answer2 = gameLoop input 2
+    printfn "The answer to part 2 is %i." answer2
     0 // return an integer exit code

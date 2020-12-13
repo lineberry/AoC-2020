2ae48a7d0ae98c5c3b708ac913051f58df04916d
Chad Lineberry <lineberry@gmail.com>
2020-12-12 20:20:18 -0500

    solve part 2 of day 12

--- Day-12/Program.fs
+++ Day-12/Program.fs
@@ -30,14 +30,28 @@ let readlines (filePath:string) =
 
 let moveFerry (ferry:Ferry) (instruction:(char*int)) =
     match instruction with
-    | ('N',_) -> { ferry with XPos = ferry.XPos + snd instruction }
-    | ('S',_) -> { ferry with XPos = ferry.XPos - snd instruction }
-    | ('E',_) -> { ferry with YPos = ferry.YPos + snd instruction }
-    | ('W',_) -> { ferry with YPos = ferry.YPos - snd instruction }
+    | ('N',y) -> { ferry with YPos = ferry.YPos + y }
+    | ('S',y) -> { ferry with YPos = ferry.YPos - y }
+    | ('E',x) -> { ferry with XPos = ferry.XPos + x }
+    | ('W',x) -> { ferry with XPos = ferry.XPos - x }
     | _ -> failwith "Encountered invalid instruction in moveFerry."
+
+let moveWaypoint (ferry:Ferry) (instruction:(char*int)) =
+    match instruction with
+    | ('N',y) -> { ferry with WaypointYPos = ferry.WaypointYPos + y }
+    | ('S',y) -> { ferry with WaypointYPos = ferry.WaypointYPos - y }
+    | ('E',x) -> { ferry with WaypointXPos = ferry.WaypointXPos + x }
+    | ('W',x) -> { ferry with WaypointXPos = ferry.WaypointXPos - x }
+    | _ -> failwith "Encountered invalid instruction in moveWaypoint."
 
 let advanceFerry (ferry:Ferry) (instruction:(char*int)) =
     moveFerry ferry (ferry.Facing,snd instruction)
+
+let advanceSystem (ferry:Ferry) (instruction:(char*int)) =
+    let timesToAdvance = snd instruction
+    let xToAdvance = ferry.WaypointXPos * timesToAdvance
+    let yToAdvance = ferry.WaypointYPos * timesToAdvance
+    { ferry with XPos = ferry.XPos + xToAdvance; YPos = ferry.YPos + yToAdvance }
 
 //Find a better algorithm for this
 let turnFerry (ferry:Ferry) (instruction:(char*int)) =
@@ -74,6 +88,13 @@ let turnFerry (ferry:Ferry) (instruction:(char*int)) =
         | _ -> ferry
     | _ -> failwith "Encountered invalid instruction in turnFerry."
 
+let rotateWaypoint (ferry:Ferry) (instruction:(char*int)) =
+    match instruction with
+    | ('L',90) | ('R',270) ->  {ferry with WaypointXPos = -1 * ferry.WaypointYPos; WaypointYPos = ferry.WaypointXPos}
+    | ('R',90) | ('L',270) -> {ferry with WaypointXPos = ferry.WaypointYPos; WaypointYPos = -1 * ferry.WaypointXPos}
+    | (_,180) -> {ferry with WaypointXPos = -1 * ferry.WaypointXPos; WaypointYPos = -1 * ferry.WaypointYPos}
+    | _ -> failwith "Encountered invalid instruction in rotateWaypoint"
+
 let getNewFerryState (ferry:Ferry) (instruction:(char*int)) =
     match instruction with
     | ('N',_) | ('S',_) | ('E',_) | ('W',_) -> moveFerry ferry instruction
@@ -81,11 +102,25 @@ let getNewFerryState (ferry:Ferry) (instruction:(char*int)) =
     | ('F',_) -> advanceFerry ferry instruction
     | (_,_) -> failwith "Encountred invalid instruction in getNewFerryState."
 
+let getNewSystemState (ferry:Ferry) (instruction:(char*int)) =
+    match instruction with
+    | ('N',_) | ('S',_) | ('E',_) | ('W',_) -> moveWaypoint ferry instruction
+    | ('L',_) | ('R',_) -> rotateWaypoint ferry instruction
+    | ('F',_) -> advanceSystem ferry instruction
+    | (_,_) -> failwith "Encountred invalid instruction in getNewSystenState."
+
 let rec eventLoop (ferry:Ferry) (instructions:list<(char*int)>) =
     match instructions with 
     | head::tail -> 
         let newFerryState = getNewFerryState ferry head
         eventLoop newFerryState tail
+    | [] -> ferry
+
+let rec systemLoop (ferry:Ferry) (instructions:list<(char*int)>) =
+    match instructions with 
+    | head::tail -> 
+        let newSystemState = getNewSystemState ferry head
+        systemLoop newSystemState tail
     | [] -> ferry
 
 [<EntryPoint>]
@@ -97,5 +132,9 @@ let main argv =
     //Solve part 1
     let finalFerryState = eventLoop startingFerryState movementInstructions
     printfn "The answer to part 1 is %i." (getManhattanDistance finalFerryState)
+
+    //Solve part 2
+    let finalSystemState = systemLoop startingFerryState movementInstructions
+    printfn "The answer to part 2 is %i." (getManhattanDistance finalSystemState)
 
     0 // return an integer exit code
